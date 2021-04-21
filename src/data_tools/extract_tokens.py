ccc7596a466c9beef12d35ddc59bf4df9b08dd58
chaeyoung-lee <cylee7133@gmail.com>
2021-04-21 09:58:33 +0900

    token map from gt.txt

--- src/data_tools/extract_tokens.py
+++ src/data_tools/extract_tokens.py
@@ -1,27 +1,10 @@
 import argparse
 import csv
+import sys
 
 
 def parse_symbols(truth):
-    unique_symbols = set()
-    i = 0
-    while i < len(truth):
-        char = truth[i]
-        i += 1
-        if char.isspace():
-            continue
-        elif char == "\\":
-            if truth[i] == "{" or truth[i] == "}":
-                unique_symbols.add(char + truth[i])
-                i += 1
-                continue
-            escape_seq = char
-            while i < len(truth) and truth[i].isalpha():
-                escape_seq += truth[i]
-                i += 1
-            unique_symbols.add(escape_seq)
-        else:
-            unique_symbols.add(char)
+    unique_symbols = set(truth.split())
     return unique_symbols
 
 
@@ -33,13 +16,10 @@ def create_tokens(groundtruth, output="tokens.txt"):
             truth_symbols = parse_symbols(truth)
             unique_symbols = unique_symbols.union(truth_symbols)
 
-        # This is somehow wrong, as it should be recognised as "less than N"
-        # It would be two symbols, which are both already present.
-        unique_symbols.remove("\\ltN")
         symbols = list(unique_symbols)
         symbols.sort()
         with open(output, "w") as output_fd:
-            writer = csv.writer(output_fd, delimiter="\t")
+            writer = csv.writer(output_fd, delimiter="\n")
             writer.writerow(symbols)
 
 
@@ -55,6 +35,6 @@ if __name__ == "__main__":
         default="tokens.txt",
         help="Output path of the tokens text file",
     )
-    parser.add_argument("groundtruth", nargs=1, help="Ground truth TSV file")
+    parser.add_argument("groundtruth", nargs=1, help="Ground truth TXT file")
     args = parser.parse_args()
     create_tokens(args.groundtruth[0], args.output)
